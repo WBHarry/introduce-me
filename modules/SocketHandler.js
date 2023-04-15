20a0da75ceeb0ba39a458db27ec95b7c09c51c8a
DarKDinDoN <maxime.chicault@gmail.com>
2023-04-15 18:11:05 +0200

    Update SocketHandler.js

--- modules/SocketHandler.js
+++ modules/SocketHandler.js
@@ -4,8 +4,9 @@ export const setupSockets = async () => {
     game.socket.on(`module.introduce-me`, async request => {
         switch(request.type){
             case RequestType.introduce:
-                const token = await fromUuid(request.data.uuid);
-                await new Introduction().introductionDisplay(token, token.actor);
+                const entity = await fromUuid(request.data.uuid);
+                const token = entity.documentName === "Actor" ? entity.prototypeToken : entity;
+                const actor = entity.documentName === "Actor" ? entity : entity.actor;
                 break;
         }
     });
